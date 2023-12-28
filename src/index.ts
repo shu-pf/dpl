01c2f6643833c1663ba3a2f022ab4146d619566d
shu-pf <s_furuse@yumemi.co.jp>
2023-12-28 14:53:02 +0900

    Update version and add command handling

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,24 @@ import { makeSettingFile } from './cmd/make-setting-file';
 import { importSetting } from './config/setting';
 
 const main = async () => {
+  /**
+   * version コマンドが指定された場合はバージョンを表示して終了
+   */
+  if (process.argv.length == 3 && process.argv[2] == 'version') {
+    console.log('1.2.0');
+    return;
+  }
+  /**
+   * setting コマンドが指定された場合は設定ファイルを作成して終了
+   */
   if (process.argv.length == 3 && process.argv[2] == 'setting') {
     await makeSettingFile();
     return;
   }
 
+  /**
+   * メイン処理
+   */
   importSetting();
 
   if (process.argv.length == 2) {
